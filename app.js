8d294693e16d92245a3cedd42dac6585eef71b30
Chidi Ekeke <chidiekeke18@yahoo.com>
2020-12-15 22:10:30 -0600

    added js and inquier

---
+++ app.js
@@ -0,0 +1,38 @@
+const inquire = require('inquirer');
+
+
+var responseM = [
+{
+    type: 'input',
+    message: 'What is your Managers name',
+    name: 'title'
+
+},
+
+{
+    type: 'input ',
+    message: 'What is your email?',
+    name: 'description',
+},
+{
+    type: 'input ',
+    message: 'What is your employee id?',
+    name: 'description',
+},
+{
+    type: 'number ',
+    message: 'What is your office number',
+    name: 'description',
+},
+{
+    type: 'confirm ',
+    message: 'What type of team member would you like to add to add in',
+    name: 'description',
+},
+
+]
+
+
+
+
+
