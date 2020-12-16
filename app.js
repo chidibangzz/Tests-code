7c5745e18ac17f4fd18e45d717b8c8abddf92c98
Chidi Ekeke <chidiekeke18@yahoo.com>
2020-12-16 16:13:32 -0600

    develop

--- app.js
+++ app.js
@@ -1,109 +1,35 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require("./manager.test");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
 
-const render = require('render');
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-var responseM = [
-    {
-        type: 'input',
-        message: 'What is your Managers name',
-        name: 'name',
-
-    },
-    {
-        type: 'input ',
-        message: 'What is your email adress?',
-        name: 'emailadress',
-    },
-    {
-        type: 'input ',
-        message: 'What is your employee id?',
-        name: 'employeeId',
-    },
-    {
-        type: 'number ',
-        message: 'What is your office number',
-        name: 'phoneNumber',
-    },
-    {
-        type: 'confirm ',
-        message: 'Which type of team member would you like to add in?',
-        name: 'newMember',
-    },
-]
-
-inquirer.prompt(responseM).then(function (answers) {
-    // console.log(answers)
-    fs.writeFile("index.html", generateMarkdown(answers), (err) => {
-        if (err) throw err;
-        console.log('This information has been saved');
-    });
-});
-
-function generateMarkdown(data) {
-    return `# ${data.title}
-    
-    ##Title 
-    
-    ${data.title}
-    
-    ##Description
-    
-    ${data.description}
-    
-    
-    ##Table of Contents
-    
-    
-    * [Installation Instructions](#Installation)
-    * [Guidelines] (#guidelines)
-    * [Test Instructions](#Tests)
-    * [License](#License)
-    * [Questions](#Questions)
-    
-    ##Installation
-    
-    ${data.Installations}
-    
-    ##Usage
-    
-    ${data.usage}
-    
-    ##Guidelines
-    
-    ${data.guidelines}
-    
-    ##Tests
-    
-    ${data.tests}
-    
-    ##Questions 
-    [Github](https://github.com/chidibangzz/Node-readme/${data.github})
-    
-    Feel free to email with questions ${data.emailadress}
-    
-    
-    
-    
-    `;
-}
+const render = require("./lib/htmlRenderer");
 
 
-module.exports = generateMarkdown;
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
 
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
 
-// //example of testing my function its saying if
-// function Arithmetic(number = 0) {
-//   this.number = number; //testing to see if this is a number 
-// }
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
 
-function askIntern() {
-    inquire.prompt({
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
 
-    }).then(internResponse) => {
-        console.log(internResponse);
-    
-        team.push(newIntern("Chidi , Ut Austin"))
-    }
-}
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
